1888132e0d958b885e79d6994dda9b36bde56f87
Pergo01 <alessandropergolizzi01@gmail.com>
2024-03-11 22:13:35 +0100

    Layout updated in home screen

--- iotail_companion/lib/UI/Material/home.dart
+++ iotail_companion/lib/UI/Material/home.dart
@@ -8,48 +8,110 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  Map<String, String> cani = {"Fido": "Golden Retriever"};
+  Map<String, String> cani = {
+    "Fido": "Golden Retriever",
+    "Fuffi": "Dobbermann"
+  };
+
+  List<String> prenotazioni = ["Casa 1", "Casa 2"];
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           const Text(
             "Cani:",
             style: TextStyle(fontSize: 40),
           ),
-          Card(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Image.asset(
-                    "assets/default_cane.jpeg",
-                    fit: BoxFit.contain,
-                    height: 100,
-                    width: 100,
+          SizedBox(
+            height: 150,
+            child: ListView.separated(
+              padding: const EdgeInsets.all(8),
+              itemCount: cani.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: SizedBox(
+                      width: 300,
+                      child: Row(
+                        children: [
+                          Container(
+                            decoration: const BoxDecoration(
+                                shape: BoxShape.rectangle,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5))),
+                            clipBehavior: Clip.hardEdge,
+                            child: Image.asset(
+                              "assets/default_cane.jpeg",
+                              height: 100,
+                              width: 100,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 8,
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(cani.keys.elementAt(index),
+                                    style: const TextStyle(
+                                        fontSize: 40,
+                                        fontWeight: FontWeight.bold)),
+                                Text(
+                                  cani.values.elementAt(index),
+                                  style: const TextStyle(fontSize: 20),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        cani.keys.elementAt(0),
-                        style: const TextStyle(fontSize: 40),
-                      ),
-                      Text(
-                        cani.values.elementAt(0),
-                        style: const TextStyle(fontSize: 20),
-                      ),
-                    ],
-                  )
-                ],
-              ),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return const SizedBox(
+                  width: 8,
+                );
+              },
             ),
           ),
+          if (prenotazioni.isNotEmpty)
+            Divider(
+              color: Theme.of(context).colorScheme.primary,
+              thickness: 2,
+            ),
+          if (prenotazioni.isNotEmpty)
+            const Text(
+              "Prenotazioni:",
+              style: TextStyle(fontSize: 40),
+            ),
+          Expanded(
+            child: ListView.separated(
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: SizedBox(
+                      height: 100,
+                      width: double.infinity,
+                      child: Text(prenotazioni[index]),
+                    ),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return const SizedBox(
+                    height: 8,
+                  );
+                },
+                itemCount: prenotazioni.length),
+          )
         ],
       ),
     );
