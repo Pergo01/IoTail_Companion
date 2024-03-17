b05ea29271218fc4f6ffba5bc147d6e2cc7a78a2
claudiosul <klaudio98@hotmail.it>
2024-03-17 21:41:08 +0100

    new center button

---
+++ iotail_companion/lib/center_button/widgets/floating_center_button_child.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../constants/dimens.dart';
+import 'animated_button.dart';
+import '../util/event_bus.dart';
+
+class FloatingCenterButtonChild extends StatelessWidget {
+  const FloatingCenterButtonChild({required this.child, this.onTap, Key? key})
+      : super(key: key);
+  final Widget child;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        AnimatedButtonState.eventBus.sendEvent(ItemClickBusEvent());
+        onTap?.call();
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(Dimens.buttonContentPadding),
+        child: CircleAvatar(
+          backgroundColor:
+              Colors.white.withOpacity(Dimens.buttonContentOpacityValue),
+          radius: Dimens.circularButtonContentRadius,
+          child: child,
+        ),
+      ),
+    );
+  }
+}
