956dfe6128b387e35b473f97b3a875b20d4f6283
Pergo01 <alessandropergolizzi01@gmail.com>
2024-03-22 22:43:28 +0100

    Removed spiderify cluster

--- iotail_companion/lib/UI/Material/map.dart
+++ iotail_companion/lib/UI/Material/map.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
-import 'package:mqtt5_client/mqtt5_server_client.dart';
 import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:mqtt5_client/mqtt5_server_client.dart';
 
 class Map extends StatefulWidget {
   final MqttServerClient client;
@@ -22,6 +22,8 @@ class _MapState extends State<Map> {
   late AlignOnUpdate _alignPositionOnUpdate;
   late final StreamController<double?> _alignPositionStreamController;
   late List<Marker> markers;
+
+  final MapController mapController = MapController();
 
   @override
   void initState() {
@@ -46,51 +48,52 @@ class _MapState extends State<Map> {
         height: 30,
         width: 30,
         point: const LatLng(44.88635, 7.33861),
-        child: Icon( 
+        child: Icon(
           Icons.pets,
           color: Theme.of(context).colorScheme.primary,
-          ),
+        ),
       ),
       Marker(
         alignment: Alignment.center,
         height: 30,
         width: 30,
         point: const LatLng(44.88487, 7.33523),
-        child: Icon( 
+        child: Icon(
           Icons.pets,
           color: Theme.of(context).colorScheme.primary,
-          ),
+        ),
       ),
       Marker(
         alignment: Alignment.center,
         height: 30,
         width: 30,
         point: const LatLng(44.881874, 7.331156),
-        child: Icon( 
+        child: Icon(
           Icons.pets,
           color: Theme.of(context).colorScheme.primary,
-          ),
+        ),
       ),
       Marker(
         alignment: Alignment.center,
         height: 30,
         width: 30,
         point: const LatLng(44.88601, 7.33707),
-        child: Icon( 
+        child: Icon(
           Icons.pets,
           color: Theme.of(context).colorScheme.primary,
-          ),
+        ),
       ),
     ];
     return Center(
       child: PopupScope(
         popupController: _popupController,
         child: FlutterMap(
+            mapController: mapController,
             options: MapOptions(
-              initialCenter: const LatLng(44.890014050623655, 7.356047819388321),
+              initialCenter:
+                  const LatLng(44.890014050623655, 7.356047819388321),
               initialZoom: 16.5,
-              onTap: (_, __) => _popupController
-                .hideAllPopups(),
+              onTap: (_, __) => _popupController.hideAllPopups(),
             ),
             children: [
               TileLayer(
@@ -99,7 +102,8 @@ class _MapState extends State<Map> {
               ),
               MarkerClusterLayerWidget(
                 options: MarkerClusterLayerOptions(
-                  spiderfyCircleRadius: markers.length*20,
+                  spiderfyCluster: false,
+                  spiderfyCircleRadius: markers.length * 20,
                   spiderfySpiralDistanceMultiplier: 2,
                   circleSpiralSwitchover: 12,
                   maxClusterRadius: 120,
@@ -109,6 +113,15 @@ class _MapState extends State<Map> {
                   padding: const EdgeInsets.all(50),
                   maxZoom: 15,
                   markers: markers,
+                  onClusterTap: (cluster) {
+                    final latLngs =
+                        cluster.markers.map((m) => m.point).toList();
+                    final bounds = LatLngBounds.fromPoints(latLngs);
+                    mapController.fitCamera(
+                      CameraFit.bounds(
+                          bounds: bounds, padding: const EdgeInsets.all(50)),
+                    );
+                  },
                   polygonOptions: PolygonOptions(
                       borderColor: Theme.of(context).colorScheme.tertiary,
                       color: Colors.black12,
@@ -119,7 +132,8 @@ class _MapState extends State<Map> {
                       //provare qua a riposizionare i popup
                       markerTapBehavior: MarkerTapBehavior.custom(
                         (popupSpec, popupState, popupController) {
-                          if (popupState.selectedPopupSpecs.contains(popupSpec)) {
+                          if (popupState.selectedPopupSpecs
+                              .contains(popupSpec)) {
                             popupController.hideAllPopups();
                           } else {
                             popupController.showPopupsOnlyForSpecs([popupSpec]);
@@ -128,37 +142,41 @@ class _MapState extends State<Map> {
                       ),
                       popupController: _popupController,
                       popupBuilder: (_, marker) => Animate(
-                        effects: const [
-                          SlideEffect(
-                              begin: Offset(0, 0.2),
-                              end: Offset(0, 0),
-                              duration: Duration(milliseconds: 300),
-                              curve: Curves.easeIn,
-                          ),
-                        ],
-
-                        child: Container(
-                              width: 200,
-                              height: 100,
-                              color: Colors.white,
-                              child: GestureDetector(
-                                onTap: () => _popupController.hideAllPopups(),
-                                child: const Text(
-                                  'Container popup for marker',
+                            effects: const [
+                              SlideEffect(
+                                begin: Offset(0, 0.2),
+                                end: Offset(0, 0),
+                                duration: Duration(milliseconds: 300),
+                                curve: Curves.easeIn,
+                              ),
+                            ],
+                            child: Card(
+                              elevation: 3,
+                              child: Container(
+                                width: 200,
+                                height: 100,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: GestureDetector(
+                                  onTap: () => _popupController.hideAllPopups(),
+                                  child: const Text(
+                                    'Container popup for marker',
+                                  ),
                                 ),
                               ),
                             ),
-                      )),
+                          )),
                   builder: (context, markers) {
                     return Container(
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Theme.of(context).colorScheme.tertiary
-                      ),
+                          borderRadius: BorderRadius.circular(20),
+                          color: Theme.of(context).colorScheme.tertiary),
                       child: Center(
                         child: Text(
                           markers.length.toString(),
-                          style: TextStyle(color: Theme.of(context).colorScheme.onTertiary),
+                          style: TextStyle(
+                              color: Theme.of(context).colorScheme.onTertiary),
                         ),
                       ),
                     );
